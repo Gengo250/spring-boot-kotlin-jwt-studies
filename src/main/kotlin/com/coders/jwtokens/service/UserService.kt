1af8780ebd713b31903790fc9b8414814fc5c79f
Gengo250 <miguelgengo28@gmail.com>
2026-02-26 08:04:17 -0300

    update classe's

--- src/main/kotlin/com/coders/jwtokens/service/UserService.kt
+++ src/main/kotlin/com/coders/jwtokens/service/UserService.kt
@@ -1,11 +1,28 @@
 package com.coders.jwtokens.service;
 
+import com.coders.jwtokens.model.User
 import com.coders.jwtokens.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import java.util.UUID
 
 @Service
 public class UserService (
     private val userRepository :UserRepository
 ){
+    fun createUser(user : User): User? {
+        val found = userRepository.findByEmail(user.email)
+        return if(found == null){
+            userRepository.save(user)
+            user
+        } else null
+    }
+
+    fun findByUUID(uuid: UUID) : User? =
+        userRepository.findByUUID(uuid)
+    fun findByAll() : List<User> =
+        userRepository.findAll()
+    fun deleteByUUID(uuid: UUID): Boolean =
+        userRepository.deleteByUUID(uuid)
+
 
 }
